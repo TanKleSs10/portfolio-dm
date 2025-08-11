d24fc0eebc023c733fce4543eca5b7dbc69ee487
Diego Meza <diegoerik_mh@hotmail.com>
2025-08-11 11:15:31 -0600

    feat: is added CV

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -15,12 +15,12 @@ export default function Sidebar({ locale }: { locale: locale }) {
       className="bg-night-800 p-5 flex flex-col gap-5 rounded-lg md:w-[320px]
   md:fixed md:top-1/2 md:-translate-y-1/2  shadow-2xl z-50 @container"
     >
-      <figure className="flex flex-col items-center">
-        <h1 className="text-5xl font-unbounded font-bold text-brand-indianRed">
+      <figure className="flex flex-col">
+        <h1 className="text-5xl mb-5 font-unbounded font-bold text-brand-indianRed">
           Diego
         </h1>
-        <picture className="inline-block w-[140px] h-[140px] bg-platinum-100"></picture>
-        <h1 className="text-5xl font-unbounded font-bold text-brand-indianRed">
+        <picture className="hidden w-[140px] h-[140px] bg-platinum-100"></picture>
+        <h1 className="text-5xl text-end font-unbounded font-bold text-brand-indianRed">
           Meza
         </h1>
       </figure>
@@ -45,7 +45,11 @@ export default function Sidebar({ locale }: { locale: locale }) {
           <InstagramIcon className="w-6 h-6" />
         </SocialLink>
       </div>
-      <button className="w-full group min-h-10 relative flex bg-platinum-300 py-2 font-unbounded font-bold text-night-800 rounded cursor-pointer hover:bg-brand-folly transition-colors duration-300">
+      <a
+        href="/DiegoMeza_CV.pdf"
+        target="_blank"
+        className="w-full group min-h-10 relative flex bg-platinum-300 py-2 font-unbounded font-bold text-night-800 rounded cursor-pointer hover:bg-brand-folly transition-colors duration-300"
+      >
         <span className="absolute top-0 translate-y-1/3 left-1/2 -translate-x-1/2 group-hover:translate-x-0 group-hover:left-0 mx-2 transition-all duration-300">
           {t.viewCV[locale]}
         </span>
@@ -53,7 +57,7 @@ export default function Sidebar({ locale }: { locale: locale }) {
           strokeWidth={3}
           className="absolute top-0 translate-y-1/3 right-1/2 -translate-1/2 opacity-0 group-hover:opacity-100 group-hover:right-0 transition-all duration-300"
         />
-      </button>
+      </a>
     </aside>
   );
 }
